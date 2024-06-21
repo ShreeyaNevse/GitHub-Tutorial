60a148cce6f0250372a316d76e1755dd6022513c
Shreeya Nevse <shreeyas108@icloud.com>
2024-06-21 12:06:21 -0500

    Initial Commit

---
+++ jun/junApp.swift
@@ -0,0 +1,17 @@
+//
+//  junApp.swift
+//  jun
+//
+//  Created by Shreeya Nevse on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct junApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
